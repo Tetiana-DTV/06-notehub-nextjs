dce07cb71a8a4689dddcf2571c8e4c52a87a85b7
Tetiana <dak7214@gmail.com>
2025-11-03 23:36:03 +0200

    update api and add loader.tsx

--- lib/api.ts
+++ lib/api.ts
@@ -2,14 +2,6 @@ import axios from "axios";
 import type { Note } from "../types/note";
 
 axios.defaults.baseURL = "https://notehub-public.goit.study/api";
-const apiKey = process.env.NEXT_PUBLIC_NOTEHUB_TOKEN;
-
-const options = {
-  headers: {
-    accept: "application/json",
-    Authorization: `Bearer ${apiKey}`,
-  },
-};
 
 interface FetchNotesProps {
   notes: Note[];
@@ -26,6 +18,13 @@ export async function fetchNotes(
   query: string,
   page: number
 ): Promise<FetchNotesProps> {
+  const apiKey = process.env.NEXT_PUBLIC_NOTEHUB_TOKEN;
+  const options = {
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${apiKey}`,
+    },
+  };
   const perPage = 12;
   const res = await axios.get<FetchNotesProps>(
     `notes?search=${query}&page=${page}&perPage=${perPage}`,
@@ -35,16 +34,37 @@ export async function fetchNotes(
 }
 
 export async function createNote(newNote: NewNote): Promise<Note> {
+  const apiKey = process.env.NEXT_PUBLIC_NOTEHUB_TOKEN;
+  const options = {
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${apiKey}`,
+    },
+  };
   const res = await axios.post<Note>(`notes`, newNote, options);
   return res.data;
 }
 
 export async function deleteNote(id: string): Promise<Note> {
+  const apiKey = process.env.NEXT_PUBLIC_NOTEHUB_TOKEN;
+  const options = {
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${apiKey}`,
+    },
+  };
   const res = await axios.delete<Note>(`/notes/${id}`, options);
   return res.data;
 }
 
 export async function fetchNoteById(id: string): Promise<Note> {
+  const apiKey = process.env.NEXT_PUBLIC_NOTEHUB_TOKEN;
+  const options = {
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${apiKey}`,
+    },
+  };
   const res = await axios.get<Note>(`/notes/${id}`, options);
   return res.data;
 }
