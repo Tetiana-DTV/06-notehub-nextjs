0cb82aa40131a131751ba49e0eef491fc382618c
Tetiana <dak7214@gmail.com>
2025-11-11 00:39:56 +0200

    update lib

--- lib/api.ts
+++ lib/api.ts
@@ -1,70 +1,61 @@
 import axios from "axios";
 import type { Note } from "../types/note";
 
+// Базова URL для всіх запитів
 axios.defaults.baseURL = "https://notehub-public.goit.study/api";
+
+// Функція для динамічного отримання заголовків з токеном
+const getOptions = () => ({
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${process.env.NEXT_PUBLIC_NOTEHUB_TOKEN}`,
+  },
+});
 
 interface FetchNotesProps {
   notes: Note[];
   totalPages: number;
 }
 
-interface NewNote {
+export interface NewNote {
   title: string;
   content: string;
   tag: "Todo" | "Work" | "Personal" | "Meeting" | "Shopping";
 }
 
+// Отримати список нотаток
 export async function fetchNotes(
   query: string,
-  page: number
+  page: number,
+  tag?: string
 ): Promise<FetchNotesProps> {
-  const apiKey = process.env.NEXT_PUBLIC_NOTEHUB_TOKEN;
-  const options = {
-    headers: {
-      accept: "application/json",
-      Authorization: `Bearer ${apiKey}`,
-    },
-  };
   const perPage = 12;
-  const res = await axios.get<FetchNotesProps>(
-    `notes?search=${query}&page=${page}&perPage=${perPage}`,
-    options
-  );
+  const res = await axios.get<FetchNotesProps>("notes", {
+    ...getOptions(),
+    params: {
+      search: query,
+      page,
+      perPage,
+      tag,
+    },
+  });
   return res.data;
 }
 
+// Створити нову нотатку
 export async function createNote(newNote: NewNote): Promise<Note> {
-  const apiKey = process.env.NEXT_PUBLIC_NOTEHUB_TOKEN;
-  const options = {
-    headers: {
-      accept: "application/json",
-      Authorization: `Bearer ${apiKey}`,
-    },
-  };
-  const res = await axios.post<Note>(`notes`, newNote, options);
+  const res = await axios.post<Note>("notes", newNote, getOptions());
   return res.data;
 }
 
+// Видалити нотатку за ID
 export async function deleteNote(id: string): Promise<Note> {
-  const apiKey = process.env.NEXT_PUBLIC_NOTEHUB_TOKEN;
-  const options = {
-    headers: {
-      accept: "application/json",
-      Authorization: `Bearer ${apiKey}`,
-    },
-  };
-  const res = await axios.delete<Note>(`/notes/${id}`, options);
+  const res = await axios.delete<Note>(`notes/${id}`, getOptions());
   return res.data;
 }
 
+// Отримати нотатку за ID
 export async function fetchNoteById(id: string): Promise<Note> {
-  const apiKey = process.env.NEXT_PUBLIC_NOTEHUB_TOKEN;
-  const options = {
-    headers: {
-      accept: "application/json",
-      Authorization: `Bearer ${apiKey}`,
-    },
-  };
-  const res = await axios.get<Note>(`/notes/${id}`, options);
+  const res = await axios.get<Note>(`notes/${id}`, getOptions());
   return res.data;
 }
