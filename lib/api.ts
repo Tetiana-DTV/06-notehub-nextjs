b755c2dcf995c0355c559c3eecdd4aad05cef4c6
Tetiana <dak7214@gmail.com>
2025-11-11 00:41:47 +0200

    update lib

--- lib/api.ts
+++ lib/api.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 import type { Note } from "../types/note";
 
-// Базова URL для всіх запитів
+
 axios.defaults.baseURL = "https://notehub-public.goit.study/api";
 
-// Функція для динамічного отримання заголовків з токеном
+
 const getOptions = () => ({
   headers: {
     accept: "application/json",
@@ -23,7 +23,7 @@ export interface NewNote {
   tag: "Todo" | "Work" | "Personal" | "Meeting" | "Shopping";
 }
 
-// Отримати список нотаток
+
 export async function fetchNotes(
   query: string,
   page: number,
@@ -42,19 +42,19 @@ export async function fetchNotes(
   return res.data;
 }
 
-// Створити нову нотатку
+
 export async function createNote(newNote: NewNote): Promise<Note> {
   const res = await axios.post<Note>("notes", newNote, getOptions());
   return res.data;
 }
+ 
 
-// Видалити нотатку за ID
 export async function deleteNote(id: string): Promise<Note> {
   const res = await axios.delete<Note>(`notes/${id}`, getOptions());
   return res.data;
 }
 
-// Отримати нотатку за ID
+
 export async function fetchNoteById(id: string): Promise<Note> {
   const res = await axios.get<Note>(`notes/${id}`, getOptions());
   return res.data;
